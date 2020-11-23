4b427f3e967bd972564cb225eac5fe39dee12da7
Josh Soref <jsoref@users.noreply.github.com>
2020-11-23 03:33:33 -0500

    spelling: exponent

--- src/mongo/db/storage/key_string.cpp
+++ src/mongo/db/storage/key_string.cpp
@@ -2115,7 +2115,7 @@ Decimal128 adjustDecimalExponent(TypeBits::Reader* typeBits, Decimal128 num) {
     // The last 6 bits of the exponent are stored in the type bits. First figure out if the exponent
     // of 'num' is too high or too low. Even for a non-zero number with only a single significant
     // digit, there are only 34 possiblities while exponents with the given low 6 bits are spaced
-    // (1 << 6) == 64 apart. This is not quite enough to figure out whether to shift the expnent up
+    // (1 << 6) == 64 apart. This is not quite enough to figure out whether to shift the exponent up
     // or down when the difference is for example 32 in either direction. However, if the high part
     // of the coefficient is zero, the coefficient can only be scaled down by up to 1E19 (increasing
     // the exponent by 19), as 2**64 < 1E20. If scaling down to match the higher exponent isn't
